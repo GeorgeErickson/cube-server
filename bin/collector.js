cba9a3dbcecf7c973bcc7f678252a98b842e6f4d
GeorgeErickson <george55@mit.edu>
2013-10-07 19:43:41 -0400

    Not neccecary

--- bin/collector.js
+++
@@ -1,9 +0,0 @@
-var options = require("../config"),
-    cube = require("cube"),
-    server = cube.server(options);
-
-server.register = function(db, endpoints) {
-  cube.collector.register(db, endpoints);
-};
-
-server.start();
