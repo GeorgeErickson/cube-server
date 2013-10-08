6f4ede9067a6dcd05e4da9f8ae737b542009a01d
GeorgeErickson <george55@mit.edu>
2013-10-07 20:06:10 -0400

    Fixes

--- setup.sh
+++
@@ -1,5 +0,0 @@
-# heroku apps:create cube-collector-drinkin --remote collector
-# heroku apps:create cube-evaluator-drinkin --remote evaluator
-
-heroku config:set CUBE_TYPE=evaluator -a cube-evaluator-drinkin
-heroku config:set CUBE_TYPE=collector -a cube-collector-drinkin
